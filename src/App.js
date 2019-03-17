ce1ff4dc43f034e78fe09f3c3f927ebe81ec005e
borgatajunky <mad1083@gmail.com>
2019-03-17 19:58:00 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 
 class App extends Component {
+
+  state = {
+    show: false
+  }
+
+  popup = () => {
+    this.setState(prevState => ({ show: !prevState.show }))
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="">
+        <h2>Popup</h2>
+        <div 
+          className="popup" 
+          onMouseOver={this.popup}
+          onMouseOut={this.popup}>Click me to toggle the popup!
+          <span className={this.state.show ? 'show popuptext' : 'popuptext' } id="myPopup">A Simple Popup!</span>
+        </div>
       </div>
     );
   }
